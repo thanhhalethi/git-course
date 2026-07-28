b54d2b6aeb3d2e25f4e796ac4148e373606a31ee
haltt <thanhhalethi19@gmail.com>
2026-07-28 10:36:38 +0700

    add exercise ts

---
+++ ex1.js
@@ -0,0 +1,63 @@
+
+// const number = 12;
+// var name = "my number";
+// var isEven = false;
+
+// if (number % 2 === 0) {
+//     isEven = true;
+//     console.log(isEven)
+// }
+
+// const number1 = 0;
+// if (number1 > 0) {
+//     console.log("gia tri ban nhap la so duong");
+// } else if (number1 == 0) {
+//     console.log("gia tri ban nhap la so 0")
+// } else {
+//     console.log("gia tri ban nhap la so am")
+// }
+
+// var height = 160;
+// console.log("your hight is " + height)
+// var idealweight = (height - 100) * 9 / 10;
+// var maxWeight = height - 100;
+// var minWeight = (height - 100) * 8 / 10;
+
+// console.log("your ideal weight is " + idealweight)
+// console.log("your max weight is " + maxWeight)
+// console.log("your min weight is " + minWeight)
+
+
+// console.log(`ideal weight ${idealweight}`)
+
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(`${i} la so chan`)
+//     } else (
+//         console.log("i la so le")
+//     )
+// }
+
+// for (var i = 12; i <= 30; i++) {
+//     console.log(`${i}`)
+// }
+
+// for (var i = 1000; i <= 2000; i++) {
+//     if (i % 3 == 0) { console.log(`${i}`) }
+// }
+
+// for (var i = 1; i <= 100; i = i + 4) {
+//     console.log(i);
+// }
+
+// for (var i = 25; i >= 12; i--) {
+//     console.log(i)
+// }
+
+function testlinhtinh(n) {
+    for (var i = 0; i <= n; i++) {
+        console.log(i)
+    }
+}
+
+testlinhtinh(3);
